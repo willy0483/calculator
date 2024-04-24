b49802778a08c4988d4b4b059b44afbe1b4ab6e2
willy0483 <145011279+willy0483@users.noreply.github.com>
2024-04-24 12:19:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Valuta omregner
+  ## Funktionskrav
+    Du skal kunne taste et beløb i Dkkr i et input felt
+    Du skal kunne vælge mindst 3 valutaer fra en dropdown menu
+    der skal være en knap med teksten beregn, der trigger en beregning af valuta baseret på input. 
+    resultatet skal vises i et dom element.
+
+  Design gennemgåes af din underviser.
+
+  
---
+++ assets/js/site.js
@@ -0,0 +1,9 @@
+/*  gode ideer og tips:
+
+hvis du vil begrænse antallet af decimaler på dit resultat, så brug methoden toFixed(antal decimaler)  eks.  result.toFixed(2) giver et resultat med 2 decimaler
+
+hvis du vil have navnet på din valuta med fra options i dit select tag, så undersøg denne linje...
+ let myCurrency = mySelectElement.options[mySelectElement.selectedIndex].innerText
+ prøv evt. at consol logge mySelectElement.options, hvor mySelectElement er det select element du har fundet i din DOM med getElementById()
+
+ */
